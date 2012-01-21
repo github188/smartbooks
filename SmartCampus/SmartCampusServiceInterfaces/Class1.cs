65ffa82b14b6f45fcfdd6e53cb83f6aaaa86c282
w549015917 <w549015917@gmail.com>
2012-01-21 03:16:49 +0000

    导入SmartCampus智慧校园项目源代码。

---
+++ SmartCampus/SmartCampusServiceInterfaces/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartCampus.ServiceInterfaces
+{
+    public class Class1
+    {
+    }
+}
---
+++ SmartCampus/SmartCampusServiceInterfaces/IService.cs
@@ -0,0 +1,21 @@
+﻿//调用示例IAreaService area = ComponentFactory<IAreaService>.GetBLLPlugin();
+namespace SmartCampus.ServiceInterfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// 服务接口基类
+    /// </summary>
+    public interface IService
+    {
+        /// <summary>
+        /// 服务描述
+        /// </summary>
+        string Description
+        {
+            get;
+        }
+    }
+}
