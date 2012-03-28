21a9297717a12663c520b0f9d13f50d52742a018
w549015917 <w549015917@gmail.com>
2012-03-28 10:14:54 +0000

    加入电子邮件发送功能和短信发送功能。

--- SmartPoms/SmartPoms/Poms/Ascx/MessageEmail.ascx
+++ SmartPoms/SmartPoms/Poms/Ascx/MessageEmail.ascx
@@ -1,1 +1,32 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="MessageEmail.ascx.cs" Inherits="SmartPoms.Poms.Ascx.MessageEmail" %>
+
+<asp:Panel ID="Panel1" runat="server" GroupingText="发送邮件">
+    <table>
+        <tr>
+            <td><asp:Label ID="Label1" runat="server" Text="接收地址" ></asp:Label></td>
+            <td>
+                <asp:TextBox ID="txtEmailAddress" runat="server" 
+                    TextMode="MultiLine" Width="400" Height="35">
+                </asp:TextBox>
+            </td>
+            <td>
+                <asp:RegularExpressionValidator ID="RegularExpressionValidator1" 
+                    runat="server" 
+                    ErrorMessage="RegularExpressionValidator" 
+                    ControlToValidate="txtEmailAddress" 
+                    ValidationExpression="\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*">请输入正确的Email地址
+                </asp:RegularExpressionValidator>
+            </td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="Label2" runat="server" Text="邮件内容"></asp:Label></td>
+            <td><asp:TextBox ID="txtEmailContent" runat="server" TextMode="MultiLine" Width="400" Height="75"></asp:TextBox></td>
+            <td><asp:Label ID="lblError" runat="server" Text="" ForeColor="Red"></asp:Label></td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <asp:Button ID="btnSend" runat="server" Text="发送" onclick="btnSend_Click" />
+            </td>
+        </tr>
+    </table>
+</asp:Panel>
--- SmartPoms/SmartPoms/Poms/Ascx/MessageEmail.ascx.cs
+++ SmartPoms/SmartPoms/Poms/Ascx/MessageEmail.ascx.cs
@@ -4,11 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net;
 
 namespace SmartPoms.Poms.Ascx {
     public partial class MessageEmail : System.Web.UI.UserControl {
         protected void Page_Load(object sender, EventArgs e) {
+            if (!IsPostBack) {
+                this.lblError.Text = "";
+            }
+        }
 
+        protected void btnSend_Click(object sender, EventArgs e) {
+            this.lblError.Text = "";
+
+            /*校验输入*/
+            if (!CheckInput()) {
+                //设置邮件内容
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("", "超级管理员"); //发件人地址
+                mail.To.Add(txtEmailAddress.Text);  //收件人
+                mail.Subject = "新邮件通知:" + DateTime.Now.ToLongDateString();  //标题
+                mail.Body = txtEmailContent.Text; //正文
+                
+
+                /*发送邮件*/
+                SmtpClient smtp = new SmtpClient();
+                smtp.Credentials = new NetworkCredential("user", "pwd");
+                smtp.Host = "";
+                smtp.Port = 21;
+                smtp.Send(mail);
+            }
+        }
+
+        private bool CheckInput() {
+            if (string.IsNullOrEmpty(txtEmailAddress.Text)) {
+                this.lblError.Text = "邮件接收地址不能为空";
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtEmailContent.Text)) {
+                this.lblError.Text = "邮件内容不能为空";
+                return false;
+            }
+
+            return true;
         }
     }
 }
