3468b90f489dbfe1f733d864595925a66448ed42
w549015917 <w549015917@gmail.com>
2012-05-03 03:07:51 +0000

    加入第三方控件

---
+++ SmartThird/河南交通厅高管局网站/08河南省高管局路政信息管理系统/SmartHydUI/BaseUserControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace SmartHyd.UI {
+    public class BaseUserControl : System.Web.UI.UserControl {
+    }
+}
