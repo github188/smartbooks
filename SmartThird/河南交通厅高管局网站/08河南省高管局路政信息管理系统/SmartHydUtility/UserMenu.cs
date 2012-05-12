e413e461c1868f2979373d3e304f4335d321e08f
w549015917 <w549015917@gmail.com>
2012-05-12 11:26:19 +0000

    增加用户SessionBox类

---
+++ SmartThird/河南交通厅高管局网站/08河南省高管局路政信息管理系统/SmartHydUtility/UserMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartHyd.Utility {
+    public class UserMenu : Entity.BASE_MENU {
+        private List<Entity.BASE_ACTION> _UserAction = new List<Entity.BASE_ACTION>();
+
+        /// <summary>
+        /// 用户操作权限集合
+        /// </summary>
+        public List<Entity.BASE_ACTION> UserAction {
+            get {
+                return _UserAction;
+            }
+            set {
+                _UserAction = value;
+            }
+        }
+    }
+}
---
+++ SmartThird/河南交通厅高管局网站/08河南省高管局路政信息管理系统/SmartHydUtility/UserSession.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace SmartHyd.Utility {
+    /// <summary>
+    /// 当前登录用户Session信息
+    /// 包含用户：权限、角色、功能、部门、基本信息、动作
+    /// </summary>
+    public class UserSession : Entity.BASE_USER {
+        private List<UserRole> _UserRole = new List<UserRole>();        
+        private List<Entity.BASE_DEPT> _Department = new List<Entity.BASE_DEPT>();
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="username">用户名称</param>
+        public UserSession(int userId) {
+            BindingUserInfo(userId);
+        }
+
+        /// <summary>
+        /// 绑定用户信息
+        /// </summary>
+        /// <param name="userId">用户ID</param>
+        private void BindingUserInfo(int userId) {
+            BLL.BASE_USER_ROLE userRoleBLL = new BLL.BASE_USER_ROLE();
+            var dt = userRoleBLL.GetList(userId);    //获取用户权限集合
+
+            #region 绑定用户基本信息
+            BLL.BASE_USER userBLL = new BLL.BASE_USER();
+            Entity.BASE_USER userModel = new Entity.BASE_USER();
+            userModel = userBLL.GetUser(userId);
+            this.BIRTHDAY = userModel.BIRTHDAY;
+            this.DEGREE = userModel.DEGREE;
+            this.DEPTID = userModel.DEPTID;
+            this.FACE = userModel.FACE;
+            this.IDNUMBER = userModel.IDNUMBER;
+            this.JOBNUMBER = userModel.JOBNUMBER;
+            this.PARENTID = userModel.PARENTID;
+            this.PHONE = userModel.PHONE;
+            this.PHOTO = userModel.PHONE;
+            this.PROF = userModel.PROF;
+            this.REMARK = userModel.REMARK;
+            this.SEX = userModel.SEX;
+            this.STSTUS = userModel.STSTUS;
+            this.USERID = userModel.USERID;
+            this.USERNAME = userModel.USERNAME;
+            this.USERPWD = userModel.USERPWD;
+            #endregion
+
+            #region 绑定部门信息
+            BLL.BASE_DEPT deptBLL = new BLL.BASE_DEPT();
+            DataTable dt_Dept = new DataTable();
+            dt_Dept = deptBLL.GetUserWhereDepartment(userModel.USERNAME, 0);    //包含子部门
+            foreach (DataRow dr in dt_Dept.Rows) {
+                Entity.BASE_DEPT model = new Entity.BASE_DEPT();
+                model.DEPTID = Convert.ToInt32(dr["DEPTID"]);
+                model.DPTINFO = dr["DPTINFO"].ToString();
+                model.DPTNAME = dr["DPTNAME"].ToString();
+                model.PARENTID = Convert.ToInt32(dr["PARENTID"].ToString());
+                model.STATUS = Convert.ToInt32(dr["STATUS"].ToString());
+                Department.Add(model);
+            }
+            #endregion
+
+            #region 绑定角色信息
+            var roles = userRoleBLL.GetList(string.Format("USERID={0} group by ROLEID", userId.ToString()));
+            BLL.BASE_ROLE roleBll = new BLL.BASE_ROLE();
+            foreach (DataRow dr in roles.Rows) {
+                Entity.BASE_ROLE roleModel = new Entity.BASE_ROLE();                
+                roleModel = roleBll.GetEntity(Convert.ToInt32(dr["ROLEID"]));
+                
+                Utility.UserRole role = new UserRole();
+                role.ROLEID = roleModel.ROLEID;
+                role.ROLEINFO = roleModel.ROLEINFO;
+                role.ROLENAME = roleModel.ROLENAME;
+
+                #region 绑定菜单
+                var menus = userRoleBLL.GetList(string.Format("USERID={0} AND ROLEID={1}", 
+                    userId.ToString(), 
+                    roleModel.ROLEID.ToString()));
+                BLL.BASE_MENU bllMenu = new BLL.BASE_MENU();
+                foreach (DataRow menuDr in menus.Rows) {
+                    Entity.BASE_MENU menuModel = new Entity.BASE_MENU();
+                    menuModel = bllMenu.GetEntity(Convert.ToInt32(menuDr["MENUID"]));
+                    
+                    Utility.UserMenu menu = new UserMenu();
+                    menu.ICON = menuModel.ICON;
+                    menu.MENUID = menuModel.MENUID;
+                    menu.MENUINFO = menuModel.MENUINFO;
+                    menu.MENUNAME = menuModel.MENUNAME;
+                    menu.MENUURL = menuModel.MENUURL;
+                    menu.PARENTID = menuModel.PARENTID;
+                    menu.STATUS = menuModel.STATUS;
+
+                    #region 绑定菜单的Action
+                    var actions = userRoleBLL.GetList(string.Format("USERID={0} AND ROLEID={1} AND MENUID={2}",
+                        userId.ToString(),
+                        roleModel.ROLEID.ToString(),
+                        menu.MENUID.ToString()));
+                    BLL.BASE_ACTION actionBLL = new BLL.BASE_ACTION();
+                    foreach (DataRow actionDr in actions.Rows) {
+                        Entity.BASE_ACTION actionModel = new Entity.BASE_ACTION();
+                        actionModel = actionBLL.GetEntity(Convert.ToInt32(actionDr["ACTIONID"].ToString()));
+
+                        menu.UserAction.Add(actionModel);   //动作信息加载菜单中
+                    }
+                    #endregion
+
+                    role.UserMenu.Add(menu);    //菜单加入角色组中
+                }
+                #endregion
+
+                UserRole.Add(role); //角色组加入集合中
+            }
+            #endregion
+        }
+
+        /// <summary>
+        /// 用户所属部门
+        /// </summary>
+        public List<Entity.BASE_DEPT> Department {
+            get {
+                return _Department;
+            }
+            set {
+                _Department = value;
+            }
+        }
+
+        /// <summary>
+        /// 用户角色集合
+        /// </summary>
+        public List<UserRole> UserRole {
+            get {
+                return _UserRole;
+            }
+            set {
+                _UserRole = value;
+            }
+        }
+    }
+}
