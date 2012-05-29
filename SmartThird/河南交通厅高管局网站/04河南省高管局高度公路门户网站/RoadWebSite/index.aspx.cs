cb136827beba2b4e858758bf71a8ac5c1a384c72
ytsunmeihong <ytsunmeihong@gmail.com>
2012-05-29 06:51:24 +0000

    增加公告公示

--- SmartThird/河南交通厅高管局网站/04河南省高管局高度公路门户网站/RoadWebSite/index.aspx.cs
+++ SmartThird/河南交通厅高管局网站/04河南省高管局高度公路门户网站/RoadWebSite/index.aspx.cs
@@ -17,20 +17,26 @@ public partial class index : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack) { 
-          RoadDepart depart=(RoadDepart)Session["roadinfo"];
-          this.Title = "网站首页-" + depart.RD_Name + "门户网站";
+          RoadDepart depart=(RoadDepart)Session["roadinfo"];//获取一个路政单位的实体信息
+          this.Title = "网站首页门户网站";
 
-          DataTable dtLZFG = NewsInfoService.Get_NewsViewList(depart.RD_ID, 2, 7);
-          rptLZFG.DataSource = dtLZFG;
+          DataTable dtLZFG = NewsInfoService.Get_NewsViewList(depart.RD_ID, 2, 7);//2代表类型是路政法规
+          rptLZFG.DataSource = dtLZFG;//路政法规
           rptLZFG.DataBind();
 
-          DataTable dtGZDT = NewsInfoService.Get_NewsViewList(depart.RD_ID, 1, 7);
-          rptGZDT.DataSource = dtGZDT;
+          DataTable dtGZDT = NewsInfoService.Get_NewsViewList(depart.RD_ID, 1, 7);//1代表是工作动态
+          rptGZDT.DataSource = dtGZDT;//工作动态
           rptGZDT.DataBind();
 
-          DataTable dtDWRY = NewsInfoService.Get_NewsViewList(depart.RD_ID, 3, 0);
-          rptRongYu.DataSource = dtDWRY;
+          DataTable dtDWRY = NewsInfoService.Get_NewsViewList(depart.RD_ID, 3, 0);//3代表是单位荣誉
+          rptRongYu.DataSource = dtDWRY;//单位荣誉
           rptRongYu.DataBind();
+
+          DataTable dtGGGS = NewsInfoService.Get_NewsViewList(depart.RD_ID,4,7);
+          rptGGGS.DataSource = dtGGGS;
+          rptGGGS.DataBind();
+
+         
         }
     }
 }
